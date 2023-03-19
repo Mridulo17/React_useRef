e9ba301f672a312d3b7c6b5127f37b32fadade86
mridulo07 <mridul.talukder@shebatech.com.bd>
2023-03-19 16:10:17 +0600

    useRef project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+  const resultRef = useRef(null);
+  const inputRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  const plus = (e) => {
+    e.preventDefault();
+    setResult(() => result + Number(inputRef.current.value));
+  };
+
+  const minus = (e) => {
+    e.preventDefault();
+    setResult(() => result - Number(inputRef.current.value));
+  };
+
+  const times = (e) => {
+    e.preventDefault();
+    setResult(() => result * Number(inputRef.current.value));
+  };
+
+  const divide = (e) => {
+    e.preventDefault();
+    setResult(() => result / Number(inputRef.current.value));
+  };
+
+  const resetInput = (e) => {
+    e.preventDefault();
+    inputRef.current.value = 0;
+  };
+
+  const resetResult = (e) => {
+    e.preventDefault();
+    setResult((prevVal) => prevVal * 0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="">
+        <h2>Simplest Calculator</h2>
+      </div>
+      <form>
+        <p ref={resultRef}>{result}</p>
+        <input
+          pattern="[0.9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>Add</button>
+        <button onClick={minus}>Subtract</button>
+        <button onClick={times}>Multuply</button>
+        <button onClick={divide}>Divide</button>
+        <button onClick={resetInput}>Result Input</button>
+        <button onClick={resetResult}>Rest Result</button>
+      </form>
     </div>
   );
 }
